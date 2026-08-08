0460d9454c428f3abb186e5dce3cd7a73174a3a3
HungThinh <pmddhuyen@gmail.com>
2026-08-08 11:47:42 +0700

    Add on-device Unity startup diagnostics

--- MainApp/main.mm
+++ MainApp/main.mm
@@ -1,4 +1,19 @@
 #include <UnityFramework/UnityFramework.h>
+#include <cstdio>
+
+static void MUInstallRuntimeLog()
+{
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(
+        NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* documents = [paths firstObject];
+    NSString* logPath = [documents stringByAppendingPathComponent:@"MUUnity.log"];
+    freopen([logPath fileSystemRepresentation], "a", stdout);
+    freopen([logPath fileSystemRepresentation], "a", stderr);
+    setvbuf(stdout, nullptr, _IONBF, 0);
+    setvbuf(stderr, nullptr, _IONBF, 0);
+    fprintf(stderr, "\n[MUBoot] process-start bundle=%s\n",
+        [[[NSBundle mainBundle] bundleIdentifier] UTF8String]);
+}
 
 UnityFramework* UnityFrameworkLoad()
 {
@@ -22,7 +37,11 @@ int main(int argc, char* argv[])
 {
     @autoreleasepool
     {
+        MUInstallRuntimeLog();
+        fprintf(stderr, "[MUBoot] loading-unity-framework\n");
         id ufw = UnityFrameworkLoad();
+        fprintf(stderr, "[MUBoot] unity-framework-loaded controller=%s\n",
+            [ufw appController] ? "yes" : "no");
         [ufw runUIApplicationMainWithArgc: argc argv: argv];
         return 0;
     }
